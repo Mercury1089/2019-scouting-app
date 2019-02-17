63e70148fd9f665893ca1ff50fc2e6e5e9648ef4
aschimmichanga <ashnasrivastava1@gmail.com>
2019-02-17 17:47:18 -0500

    sandstorm variable list

--- app/src/main/java/com/mercury1089/scoutingapp2019/Sandstorm.java
+++ app/src/main/java/com/mercury1089/scoutingapp2019/Sandstorm.java
@@ -5,6 +5,60 @@ import android.os.Bundle;
 
 public class Sandstorm extends AppCompatActivity {
 
+    /*
+    LEFT ROCKET
+    1 sandstormLeftRocketPanelNearT3
+2 sandstormLeftRocketPanelNearT2
+3 sandstormLeftRocketPanelNearT1
+4 sandstormLeftRocketPanelFarT3
+5 sandstormLeftRocketPanelFarT2
+6 sandstormLeftRocketPanelFarT1
+cargo variables
+7 sandstormLeftRocketCargoT3
+8 sandstormLeftRocketCargoT2
+9 sandstormLeftRocketCargoT1
+CARGO SHIP
+panel variables
+10 sandstormCargoShipPanelFront1
+11 sandstormCargoShipPanelFront2
+12 sandstormCargoShipPanelLeft1
+13 sandstormCargoShipPanelLeft2
+14 sandstormCargoShipPanelLeft3
+15 sandstormCargoShipPanelRight1
+16 sandstormCargoShipPanelRight2
+17 sandstormCargoShipPanelRight3
+cargo variables
+18 sandstormCargoShipCargoFront1
+19 sandstormCargoShipCargoFront2
+20 sandstormCargoShipCargoLeft1
+21 sandstormCargoShipCargolLeft2
+22 sandstormCargoShipCargoLeft3
+23 sandstormCargoShipCargoRight1
+24 sandstormCargoShipCargoRight2
+25 sandstormCargoShipCargoRight3
+RIGHT ROCKET
+panel variables
+26 sandstormRightRocketPanelNearT3
+27 sandstormRightRocketPanelNearT2
+28 sandstormRightRocketPanelNearT1
+29 sandstormRightRocketPanelFarT3
+30 sandstormRightRocketPanelFarT2
+31 sandstormRightRocketPanelFarT1
+cargo variables
+32 sandstormRightRocketCargoT3
+33 sandstormRightRocketCargoT2
+34 sandstormRightRocketCargoT1
+
+Repeated Variables for each buttonClick:
+MODE
+LOCATION
+WHAT
+SIDE
+NEAR FAR
+TIER
+SCORE
+
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
