d498b0ee22b87ad7a9605caecef381c59619dbc9
aschimmichanga <ashnasrivastava1@gmail.com>
2019-02-17 20:19:46 -0500

    cargo stuff layout names

--- app/src/main/java/com/mercury1089/scoutingapp2019/CustomView.java
+++ app/src/main/java/com/mercury1089/scoutingapp2019/CustomView.java
@@ -2,7 +2,6 @@ package com.mercury1089.scoutingapp2019;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
